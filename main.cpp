491068ce2c6bd131996380a61a324cd1aa35b9c7
codeReaperSL <144638711+codeReaperSL@users.noreply.github.com>
2024-01-03 11:20:08 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+
+
+class admin{
+
+   int savingintrest;
+   int overdrafthcarge;
+   
+
+
+};
+
+class employee{
+
+};
+
+class customer{
+
+};
+
+
+
+
+int main()
+{
+    string username;
+    string password;
+
+    cout<<"Enter the username ";
+    cin >> username;
+
+    cout<<"Enter the password ";
+    cin >> password;
+
+
+    cout<<username<<" "<<password;
+
+
+    return 0;
+}
